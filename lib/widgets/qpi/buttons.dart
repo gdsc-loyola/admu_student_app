6b512b67e3b9a0eb29f604c49ecffd2dbc3bdfcb
Marco <marco.francisco@obf.ateneo.edu>
2021-05-01 18:35:41 +0800

    fixed other button classes

--- lib/widgets/qpi/buttons.dart
+++ lib/widgets/qpi/buttons.dart
@@ -1,46 +1,55 @@
 import 'package:flutter/material.dart';
 
-class LongButton extends StatelessWidget {
+class LongButton extends StatefulWidget {
   final String text;
   final Color buttonColor;
   final Color textColor;
+LongButton(this.text, this.buttonColor, this.textColor); 
 
-  const LongButton(this.text, this.buttonColor, this.textColor); 
+  @override
+  _LongButtonState createState() => _LongButtonState();
+}
 
+class _LongButtonState extends State<LongButton> {
   @override
   Widget build(BuildContext context) {
     return Container(
       width: 219,
       height: 56,
       decoration: BoxDecoration(
-          color: buttonColor,
+          color: widget.buttonColor,
           borderRadius: BorderRadius.all(Radius.circular(5))),
       child: TextButton(
         onPressed: () {},
-        child: Text(text, style: TextStyle(color: textColor),),
+        child: Text(widget.text, style: TextStyle(color: widget.textColor),),
       ),
     );
   }
 }
 
-class ShortButton extends StatelessWidget {
+class ShortButton extends StatefulWidget {
   final String text;
   final Color buttonColor;
   final Color textColor;
 
-  const ShortButton(this.text, this.buttonColor, this.textColor); 
+  ShortButton(this.text, this.buttonColor, this.textColor); 
 
+  @override
+  _ShortButtonState createState() => _ShortButtonState();
+}
+
+class _ShortButtonState extends State<ShortButton> {
   @override
   Widget build(BuildContext context) {
     return Container(
       width: 128,
       height: 56,
       decoration: BoxDecoration(
-          color: buttonColor,
+          color: widget.buttonColor,
           borderRadius: BorderRadius.all(Radius.circular(5))),
       child: TextButton(
         onPressed: () {},
-        child: Text(text, style: TextStyle(color: textColor),),
+        child: Text(widget.text, style: TextStyle(color: widget.textColor),),
       ),
     );
   }
