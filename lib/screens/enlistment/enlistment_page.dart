422e72ceb26a9e31bda6981296e0d9280759a707
Marco Francisco <franciscomarco2921@gmail.com>
2021-05-19 15:20:40 +0800

    Initial commit for enlistment preparer

--- lib/screens/enlistment/enlistment_page.dart
+++ lib/screens/enlistment/enlistment_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'package:admu_student_app/screens/enlistment/enlistment_classes.dart';
+import 'package:admu_student_app/widgets/qpi/course_widget.dart';
+import 'package:admu_student_app/constants/app_colors.dart';
 
 class EnlistmentPage extends StatefulWidget {
   @override
@@ -12,18 +14,95 @@ class _EnlistmentPageState extends State<EnlistmentPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
-        children: [
-          Text('this is the enlistment page'),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(builder: (_) => EnlistmentClassesPage()),
-              );
-            },
-            child: Text('view semester'),
-          ),
-        ],
+      body: Container(
+        padding: EdgeInsets.fromLTRB(16, 48, 16, 29),
+        child: Column(
+          children: [
+            Column(
+              children: [
+                Row(
+                  children: [
+                    Text('Enlistment Preparer',
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline4
+                            .copyWith(color: AppColors.GRAY_DARK[0])),
+                    CircleAvatar(
+                      radius: 13.5,
+                    )
+                  ],
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(0, 35, 0, 24),
+                  child: Row(
+                    children: [
+                      Text(
+                        'Classes',
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline4
+                            .copyWith(color: AppColors.GRAY_DARK[0]),
+                      ),
+                      Spacer(),
+                      Container(
+                        decoration: BoxDecoration(
+                            color: AppColors.PRIMARY_LIGHT.withOpacity(0.2),
+                            borderRadius: BorderRadius.all(Radius.circular(100))),
+                        child: InkWell(
+                          onTap: () {},
+                          child: Ink(
+                            child: Container(
+                              height: 24,
+                              width: 72,
+                              child: Center(
+                                child: Text(
+                                  'Select',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .headline4
+                                      .copyWith(
+                                          fontSize: 14,
+                                          color: AppColors.PRIMARY_MAIN),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      IconButton(icon: Icon(Icons.add), onPressed: () {})
+                    ],
+                  ),
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    CourseCard(Colors.green, Colors.white, Colors.green[100],
+                        Colors.green, 'HISTO12', 3, 'A', () {}),
+                    CourseCard(Colors.green, Colors.white, Colors.green[100],
+                        Colors.green, 'HISTO12', 3, 'A', () {}),
+                  ],
+                ),
+                Container(
+                  height: 64,
+                  width: 224,
+                  decoration: BoxDecoration(
+                    color: AppColors.SECONDARY_MAIN.withOpacity(0.5),
+                    borderRadius: BorderRadius.all(Radius.circular(4))
+                  ),
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                            builder: (_) => EnlistmentClassesPage()),
+                      );
+                    },
+                    child: Text('Create Schedule', style: Theme.of(context).textTheme.bodyText1.copyWith(color: Colors.white, fontSize: 20),),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
