1132ed495cf9a4f4f93790daef9ec71798229ea8
Emman Evangelista <emmanuel.evangelista@obf.ateneo.edu>
2022-05-03 19:14:26 +0800

    event card now use new tag widget

--- lib/widgets/calendar/event_card.dart
+++ lib/widgets/calendar/event_card.dart
@@ -6,6 +6,7 @@ import 'package:admu_student_app/constants/app_effects.dart';
 import 'package:admu_student_app/models/calendar_events.dart';
 import 'package:admu_student_app/models/event.dart';
 import 'package:admu_student_app/screens/add_task.dart';
+import 'package:admu_student_app/widgets/calendar/event_tag.dart';
 import 'package:admu_student_app/widgets/circular_check_mark.dart';
 
 class EventCard extends StatelessWidget {
@@ -100,37 +101,7 @@ class EventCard extends StatelessWidget {
                   // tags
                   (event.tags == null || event.tags.isEmpty)
                       ? Container()
-                      : SizedBox(
-                          height: 24,
-                          child: ListView(
-                            shrinkWrap: true,
-                            scrollDirection: Axis.horizontal,
-                            children: [
-                              Container(
-                                padding: EdgeInsets.symmetric(horizontal: 16.0),
-                                height: 24,
-                                decoration: BoxDecoration(
-                                  color: AppColors.SECONDARY_LIGHT
-                                      .withOpacity(0.5),
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(4.0)),
-                                ),
-                                child: Center(
-                                  child: Text(
-                                    event.tags,
-                                    overflow: TextOverflow.ellipsis,
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .caption
-                                        .copyWith(
-                                            fontWeight: FontWeight.bold,
-                                            color: AppColors.SECONDARY_MAIN),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
+                      : EventTag(event.tags),
                 ],
               ),
             ),
